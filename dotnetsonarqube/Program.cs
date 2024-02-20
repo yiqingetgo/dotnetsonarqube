73fa9e6def503d61616132eb45f940bc21d31e77
yiqingetgo <yiqinlim@getgo.sg>
2024-02-20 17:45:59 +0800

    initial commit

---
+++ dotnetsonarqube/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int x = 4;
+int y = 5;
+int z = x + y;
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Z value's is {z}");
